0f020a285f30bbf04d5025ded9311387e7c2145c
Serdna20 <afgarciasa@ufpso.edu.co>
2024-03-08 11:44:03 -0500

    Added user log

--- Biblioteca/src/main/java/books/Book.java
+++ Biblioteca/src/main/java/books/Book.java
@@ -6,6 +6,8 @@ public class Book {
     int initialQuantity;
     int id;
     String genre;
+    String[] currentOwners;
+    int ownersQuanity;
     String loanDate;
     String returnDate;
     boolean available;
@@ -16,16 +18,74 @@ public class Book {
         this.id = id;
         this.genre = genre;
         available = true;
-        initialQuantity = quantity;
+        ownersQuanity = -1;
+        initialQuantity = quantity;        
+        currentOwners = new String[initialQuantity];
     }    
 
     public String getGenre() {
         return genre;
     }
-
+    
     public String getBookInfo() {
-        return title+" | "+"Cantidad:"+quantity;
+        String extraInfo = "\n";
+        if (ownersQuanity >= 0) {
+            for (int index = 0; index <= ownersQuanity; index++) {
+                extraInfo = extraInfo + currentOwners[index] + "\n";
+            }
+        }
+        return title + " | " + "Cantidad: " + quantity + " | Id: " + id + extraInfo;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    } 
+    
+    public String returnBook() {
+        String operationMessage;
+        if (quantity == initialQuantity) {
+            operationMessage = "No puede regresar un libro que no se presto.";
+        }
+        else {
+            quantity = quantity+1;
+            operationMessage = "Libro regresado con exito.";
+        }
+        updateBookStatus();
+        return operationMessage;
     }
     
+    public String lendBook(String[] loanInfo) {
+        String operationMessage;
+        if (available) {
+            quantity = quantity-1;
+            ownersQuanity = ownersQuanity+1;
+            operationMessage = "Libro prestado.";
+            updateOwners(loanInfo);
+        }
+        else {
+            operationMessage = "No se pudo prestar.";
+        }
+        updateBookStatus();
+        return operationMessage;
+    }
     
+    public void updateOwners(String[] loanInfo) {
+        currentOwners[ownersQuanity] = loanInfo[0] + " | " + loanInfo[1];
+    }    
+    
+    public void updateBookStatus() {
+        if (quantity ==  initialQuantity) {
+            available = true;
+        }
+        else if (quantity < 1) {
+            available = false;
+        }
+        else {
+            available = true;
+        }
+    }
 }
--- Biblioteca/src/main/java/nose/biblioteca/Biblioteca.java
+++ Biblioteca/src/main/java/nose/biblioteca/Biblioteca.java
@@ -7,7 +7,7 @@ public class Biblioteca {
         String booksFound = "";
         for (Book i : array) {
             if (i.getGenre().equals(genre)) {
-                booksFound = booksFound + i.getBookInfo();
+                booksFound = booksFound + i.getBookInfo() + "\n";
             }
         }
         return booksFound;
@@ -19,25 +19,21 @@ public class Biblioteca {
         Book book3 = new Book("Cronica de una muerte anunciada", 3, 2, "Novela");
         Book book4 = new Book("Guerra y paz", 3, 3, "Novela");        
         Book[] books = {book1, book2, book3, book4};
+        System.out.println(searchByGenre(books, "Novela"));
         byte menuAction;
         boolean wantsToExit = false;
         Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("¡Bienvenido a la biblioteca!\n¿Que accion desea realizar?\n1. Buscar libro por genero.\n2. Sacar libro\n3. Regresar libro.\n4. Salir.");
+        do {            
             menuAction = scanner.nextByte();
+            System.out.println("¡Bienvenido a la biblioteca!\n¿Que accion desea realizar?\n1. Buscar libro por genero.\n2. Sacar libro\n3. Regresar libro.\n4. Salir.");
             switch (menuAction) {
                 case 1:
                     System.out.println("Ingrese el genero del libro:");
                     System.out.println(searchByGenre(books, scanner.next()));
                     break;
                 case 2:
-                    String[] userInfo = new String[2];
-                    System.out.println("Ingrese su nombre:");
-                    userInfo[0] = scanner.next();
-                    System.out.println("Ingrese su codigo");
-                    userInfo[1] = scanner.next();
                     System.out.println("Ingrese la id del libro a sacar:");
-                    System.out.println(books[scanner.nextByte()].lendBook(userInfo));                    
+                    System.out.println(books[scanner.nextByte()].lendBook());
                     break;
                 case 3:
                     System.out.println("Ingrese la id del libro a regresar:");
